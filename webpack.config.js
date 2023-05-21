fafecad10180b3847fa87cf20014c95ac0e728c9
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-21 12:51:56 +0300

    feat: plugins and improvement filename

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require("path")
+const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
+    clean: true,
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
 }

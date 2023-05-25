90432c88d0c4c893921a429acd6b3645e187579e
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-25 15:27:40 +0300

    feat: tune css-modules, create App.tsx

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,15 +1,26 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 import webpack from "webpack"
+import { BuildOptions } from "./types/config"
 
-export function buildLoaders(): webpack.RuleSetRule[] {
-
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   // * For CSS, SASS, SCSS
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      MiniCssExtractPlugin.loader,         // Create style node from JS-strings
-      "css-loader",           // Translate CSS into commonJS
-      "sass-loader",          // Compiles Sass to CSS
+      // Create style node from JS-strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module")),
+            localIdentName: isDev 
+              ? '[path][name]__[local]--[hash:base64:5]' 
+              : '[hash:base64:8]'
+          },
+        },
+      },
+      "sass-loader",
     ],
   }
 
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react"
-import './Counter.scss'
+import classes from './Counter.module.scss'
 
 export const Counter = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className={classes.button}>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Counter</button>
     </div>

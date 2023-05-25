b8f0f67ab46194415a80265bd8fcc04535892226
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-25 14:25:30 +0300

    feat: install css, scss, sass in webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import webpack from "webpack"
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-  const typescriptloader = {
+
+  // * For CSS, SASS, SCSS
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",         // Create style node from JS-strings
+      "css-loader",           // Translate CSS into commonJS
+      "sass-loader",          // Compiles Sass to CSS
+    ],
+  }
+
+  // * For TypeScript
+  const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   }
 
-  return [typescriptloader]
+  return [typescriptLoader, cssLoader]
 }

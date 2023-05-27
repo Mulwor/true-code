b517685df45bafc8b57b96ee41edaeb5e57434dc
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-27 15:21:06 +0300

    feat: integration new promise in react.lazy (bad practice)

--- src/pages/AboutPage/AboutPage.async.ts
+++ src/pages/AboutPage/AboutPage.async.ts
@@ -4,4 +4,7 @@ import { lazy } from "react"
 // Она автоматически загрузит бандл, содержащий OtherComponent, когда этот компонент будет впервые отрендерен.
 // React.lazy принимает функцию, которая должна вызвать динамический import(). Результатом возвращённого Promise 
 // является модуль, который экспортирует по умолчанию React-компонент (export default).
-export const AboutPageAsync = lazy(() => import("./AboutPage"))
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    setTimeout(() => resolve(import("./AboutPage")), 1500)
+}));

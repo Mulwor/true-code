1af9013819f32f599dae559089d856b586941fd8
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-29 16:49:29 +0300

    feat: universal function that will add classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { Suspense, useContext, useState } from "react"
+import { Suspense } from "react"
 import "./styles/index.scss"
 import { Routes, Route, Link } from "react-router-dom"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
-import { Theme, ThemeContext } from "./theme/Theme.context"
 import { useTheme } from "./theme/useTheme"
+import { classNames } from "./helpers/classNames/classNames"
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Кнопелла</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>

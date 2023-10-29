7b4e00124bbb2e4c627deea9e2760a78b915bd29
ali <adigezalli@yandex.ru>
2023-10-29 17:59:20 +0300

    feat: added error-boundary with typescript

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,17 @@ import { Link } from 'react-router-dom';
 import { classNames } from 'shared/libs/classNames/classNames';
 import { Navbar } from 'widget/Navbar';
 import { Sidebar } from 'widget/Sidebar/ui/Sidebar/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { AppRouter } from './provider/router';
 import { useTheme } from './provider/ThemeProvider';
 
 function App() {
   const { theme } = useTheme();
+
+  useEffect(() => {
+    throw new Error();
+  });
+
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">

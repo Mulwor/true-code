6c1423d7dc7d4add47b8a50e661f7335318bed89
ali <adigezalli@yandex.ru>
2023-11-06 14:39:58 +0300

    reset

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button, ThemeButton } from './Button';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Text',
+  },
+};
+
+export const Clear: Story = {
+  args: {
+    children: 'Text',
+    // theme: ThemeButton.CLEAR,
+  },
+};

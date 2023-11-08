8b64c0a8faff4c597423dd7181c6ff9b7a8857fd
ali <adigezalli@yandex.ru>
2023-11-08 23:48:04 +0300

    write stories for Sidebar and fix bug to svg in storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
@@ -13,6 +13,21 @@ export default ({ config }: {config: webpack.Configuration}) => {
 
   config.resolve.modules.push(paths.src);
   config.resolve.extensions.push('.ts', '.tsx');
+
+  // Мы находим правила, которая обрабатывает свг, и если мы нашли данное правило,
+  // то берем и исключае данное правило
+  // eslint-disable-next-line no-param-reassign
+  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    if (/svg/.test(rule.test as string)) {
+      return { ...rule, exclude: /\.svg$/i };
+    }
+    return rule;
+  });
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
+
   config.module.rules.push(buildCssLoader(true));
 
   return config;
---
+++ src/widget/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/provider/ThemeProvider';
+import { Sidebar } from './Sidebar';
+
+const meta = {
+  title: 'widget/Sidebar',
+  component: Sidebar,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Sidebar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];

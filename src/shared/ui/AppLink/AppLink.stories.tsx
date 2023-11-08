ab18ea01eb8a693130356bc9da6c5695c8f022e6
ali <adigezalli@yandex.ru>
2023-11-09 00:22:13 +0300

    write stories for Applink and ErrorPage

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/provider/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { AppLink, AppLinkTheme } from './AppLink';
+
+const meta = {
+  title: 'shared/AppLink',
+  component: AppLink,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    to: '/',
+  },
+} satisfies Meta<typeof AppLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Blue: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.PRIMARY,
+  },
+};
+
+export const Green: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.SECONDARY,
+  },
+};
+
+export const Red: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.RED,
+  },
+};
+
+export const BlueDark: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.PRIMARY,
+  },
+};
+BlueDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const GreenDark: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.SECONDARY,
+  },
+};
+GreenDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const RedDark: Story = {
+  args: {
+    children: 'Text',
+    theme: AppLinkTheme.RED,
+  },
+};
+RedDark.decorators = [ThemeDecorator(Theme.DARK)];

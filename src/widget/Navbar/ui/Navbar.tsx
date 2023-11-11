500f68e18e785dd5c2c451a33a7a008d2e5d9de1
ali <adigezalli@yandex.ru>
2023-11-11 23:59:06 +0300

    refactor sidebar and added new icons

--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/libs/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import style from './Navbar.module.scss';
 
@@ -8,17 +7,9 @@ interface NavbarProps {
 }
 
 export function Navbar({ className }: NavbarProps) {
-  const { t } = useTranslation();
   return (
     <div className={classNames(style.navbar, {}, [className])}>
-      <div className={style.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={style.mainLink}>
-          {t('Главная')}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.RED} to="/about">
-          {t('О сайте')}
-        </AppLink>
-      </div>
+      <div className={style.links} />
     </div>
   );
 }

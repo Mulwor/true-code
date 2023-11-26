e699d33de55e8fac74fb63460e27abb388101012
ali <adigezalli@yandex.ru>
2023-11-26 13:01:44 +0300

    fix problem with Suspense, IS__DEV, Provider and create decorators for store and translation for form

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -20,6 +20,7 @@ export default {
   decorators: [
     StyleDecorator,
     RouterDecorator,
+    StyleDecorator,
     ThemeDecorator(Theme.LIGHT),
   ],
 };
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { LoginForm } from './LoginForm';
 
 const meta = {
@@ -13,3 +14,29 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {},
 };
+Primary.decorators = [StoreDecorator({
+  loginForm: {
+    username: '123',
+    password: 'asd',
+  },
+})];
+
+export const Error: Story = {
+  args: {},
+};
+Error.decorators = [StoreDecorator({
+  loginForm: {
+    username: '123',
+    password: 'asd',
+    error: 'ERROR',
+  },
+})];
+
+export const Loading: Story = {
+  args: {},
+};
+Loading.decorators = [StoreDecorator({
+  loginForm: {
+    isLoading: true,
+  },
+})];

54137de3278c6dc7c4553079511aad84f5cec38b
ali <adigezalli@yandex.ru>
2023-12-03 16:54:13 +0300

    update test loginByUsername

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -27,8 +27,9 @@ describe('loginByUsername.test', () => {
     getState = jest.fn();
   });
 
-  test('Создаем ассинхронную функцию с помощью экшена, потом мы его вызываем и помещаем в result', async () => {
+  test('Запрос должен выполнится успешно', async () => {
     const userValue = { username: 'Valera', id: '1' };
+    // Объявляем типы
     mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
 
     const action = loginByUsername({
@@ -39,9 +40,26 @@ describe('loginByUsername.test', () => {
 
     // Убеждаемся, что dispatch был вызыван
     expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+    // Убеждаемся в том, что dispatch был вызван 3 раза
+    expect(dispatch).toHaveBeenCalledTimes(3);
     // Запрос на сервер был действительно отправлен
     expect(mockedAxios.post).toHaveBeenCalled();
     // Ассинхронная функция сработало без ошибок
     expect(result.meta.requestStatus).toBe('fulfilled');
+    // Проверяем, что возвращаются данные о пользователе
+    expect(result.payload).toEqual(userValue);
+  });
+
+  test('Запрос должен выполнится с ошибкой', async () => {
+    mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+    const action = loginByUsername({ username: 'Valera', password: '123' });
+    const result = await action(dispatch, getState, undefined);
+
+    // Убеждаемся в том, что dispatch был вызван 2 раза в случае ошибки
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(mockedAxios.post).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('rejected');
+    // Также проверяем, что в случае ошибки payload равняется error
+    expect(result.payload).toBe('error');
   });
 });

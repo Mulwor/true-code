6bc4cbe4a8b1676ff4d16dbb9b472d0dab174f9b
ali <adigezalli@yandex.ru>
2023-12-17 23:42:10 +0300

    feat: change api-url in dev and production

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
+// Первый способ - если мы находимся в dev-режиме тогда в качестве основного url укажем 8к,
+// А если у нас режим продакшн, то укажем ссылку на продакшн
+// const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'https://production.ru';
+
+// Второй способ - переопределить страницу создаем еще одну глоб.переменную: __API__
+
 export const $api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: __API__,
   // Заголовок для получения данных авторизованным пользователям
   headers: {
     Authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),

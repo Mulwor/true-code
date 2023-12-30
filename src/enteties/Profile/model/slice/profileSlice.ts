9879a78ad58f7902c9df56d9d7fe205b7868ac2e
ali <adigezalli@yandex.ru>
2023-12-30 15:48:35 +0300

    write logic for profile

--- src/enteties/Profile/model/slice/profileSlice.ts
+++ src/enteties/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ProfileSchema } from '../types/profile';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Profile, ProfileSchema } from '../types/profile';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
 
 const initialState: ProfileSchema = {
   readonly: true,
@@ -12,6 +13,27 @@ export const profileSlice = createSlice({
   name: 'profile',
   initialState,
   reducers: {},
+  // 2. А здесь мы вызываем екстраредьюсеры, которые как раз и сохраняем полученные
+  // данный в стейт
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfileData.pending, (state) => {
+        state.error = undefined;
+        state.isLoading = true;
+      })
+      .addCase(
+        fetchProfileData.fulfilled,
+        (state, action: PayloadAction<Profile>) => {
+          state.isLoading = false;
+          // Сохраняем полученные данные от сервера в стейт
+          state.data = action.payload;
+        },
+      )
+      .addCase(fetchProfileData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { actions: profileActions } = profileSlice;
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,9 @@
-import { profileReducer } from 'enteties/Profile';
+import { fetchProfileData, profileReducer } from 'enteties/Profile';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/libs/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/libs/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/libs/hooks/useAppDispatch/useAppDispatch';
 
 const reducers: ReducersList = {
   profile: profileReducer,
@@ -13,6 +15,14 @@ interface ProfilePageProps {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
   const { t } = useTranslation();
+
+  // 3. Вызываем кастомный диспатчик, который записываем в useEffect и проверяем вызвался
+  // данные о профиле
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProfileData());
+  }, [dispatch]);
 
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>

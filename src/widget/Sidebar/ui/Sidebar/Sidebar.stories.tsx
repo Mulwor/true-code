e7131ce5f35ea5726b883eddb966da8546f627e0
ali <adigezalli@yandex.ru>
2024-01-07 17:09:14 +0300

    add decorator

--- src/widget/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widget/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/provider/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Sidebar } from './Sidebar';
 
 const meta = {
@@ -15,8 +16,31 @@ type Story = StoryObj<typeof meta>;
 export const Light: Story = {
   args: {},
 };
+Light.decorators = [
+  StoreDecorator({
+    user: {
+      authData: {},
+    },
+  }),
+];
 
 export const Dark: Story = {
   args: {},
 };
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [
+  ThemeDecorator(Theme.DARK),
+  StoreDecorator({
+    user: {
+      authData: {},
+    },
+  }),
+];
+
+export const NoAuth: Story = {
+  args: {},
+};
+NoAuth.decorators = [
+  StoreDecorator({
+    user: {},
+  }),
+];

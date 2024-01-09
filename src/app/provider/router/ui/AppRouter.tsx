39aede4175f6d4c23235838eba3808f7a5bc8bba
ali <adigezalli@yandex.ru>
2024-01-09 18:14:41 +0300

    feat: added private router for profile (bad practise)

--- src/app/provider/router/ui/AppRouter.tsx
+++ src/app/provider/router/ui/AppRouter.tsx
@@ -1,13 +1,25 @@
-import { Suspense } from 'react';
+import { getUserAuthData } from 'enteties/User';
+import { Suspense, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 function AppRouter() {
+  const isAuth = useSelector(getUserAuthData);
+
+  const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
+    if (route.authOnly && !isAuth) {
+      return false;
+    }
+
+    return true;
+  }), [isAuth]);
+
   return (
     <div className="wrapper">
       <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
+        {routes.map(({ path, element }) => (
           <Route
             key={path}
             path={path}

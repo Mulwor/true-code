a5e57d0e4d0a10ecb76f33fea17f61d11a923e3f
ali <adigezalli@yandex.ru>
2024-01-12 21:08:28 +0300

    feat: skeleton

--- src/enteties/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/enteties/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -11,6 +11,7 @@ import {
   getArticleDetailsIsLoading,
 } from 'enteties/Article/model/selectors/articleDetails';
 import { Text, TextAlign } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleleton/Skeleton';
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 import style from './ArticleDetails.module.scss';
 
@@ -28,14 +29,21 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
   const { t } = useTranslation('article');
   const dispatch = useAppDispatch();
   const data = useSelector(getArticleDetailsData);
-  const isLoading = useSelector(getArticleDetailsIsLoading);
+  // const isLoading = useSelector(getArticleDetailsIsLoading);
+  const isLoading = true;
   const error = useSelector(getArticleDetailsError);
 
   let content;
 
   if (isLoading) {
     content = (
-      <div>{t('loading')}</div>
+      <div>
+        <Skeleton className={style.avatar} width={200} height={200} border="50%" />
+        <Skeleton className={style.title} width={300} height={32} />
+        <Skeleton className={style.skeleton} width={600} height={24} />
+        <Skeleton className={style.skeleton} width="100%" height={200} />
+        <Skeleton className={style.skeleton} width="100%" height={200} />
+      </div>
     );
   } else if (error) {
     content = (
